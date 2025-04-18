8ebfdd661cb77a0c6f1fc0cb5c43f1689711d841
garethjevans <gareth@bryncynfelin.co.uk>
2025-04-18 15:22:13 +0100

    Brew formula update for scm version 0.3.3

--- Formula/scm.rb
+++ Formula/scm.rb
@@ -5,20 +5,20 @@
 class Scm < Formula
   desc "Utility to validate a component structure"
   homepage ""
-  version "0.3.1"
+  version "0.3.3"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/garethjevans/scm/releases/download/0.3.1/scm-darwin-amd64.tar.gz"
-      sha256 "d55cdcf0cc6ac412993574913aff9ce6e2e2011d4f7d593420ec20724a59b138"
+      url "https://github.com/garethjevans/scm/releases/download/0.3.3/scm-darwin-amd64.tar.gz"
+      sha256 "4f94b2f111999c94f6c832433e3715a38f57bdc934758bead78a249974ab1912"
 
       def install
         bin.install "scm"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/garethjevans/scm/releases/download/0.3.1/scm-darwin-arm64.tar.gz"
-      sha256 "7d469e9b092376bfc805ea8131a7b6bda5d140b3c0c4ab37a7f3ba8c2aa26a00"
+      url "https://github.com/garethjevans/scm/releases/download/0.3.3/scm-darwin-arm64.tar.gz"
+      sha256 "b9818d142081199383d45947e790ad5401dc2c32c10f95883502742a81caf39f"
 
       def install
         bin.install "scm"
@@ -28,19 +28,23 @@ class Scm < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/garethjevans/scm/releases/download/0.3.1/scm-linux-amd64.tar.gz"
-      sha256 "b13c0b08710f3974f11747fbd1fa6ac26a7bb06982abf5e3c82b7dd99f0884ea"
+      if Hardware::CPU.is_64_bit?
+        url "https://github.com/garethjevans/scm/releases/download/0.3.3/scm-linux-amd64.tar.gz"
+        sha256 "9cc0c0c7f1c17c5d75e85ff6b448dc8d9f7a7d564ff2034b2f634685bc8afcc3"
 
-      def install
-        bin.install "scm"
+        def install
+          bin.install "scm"
+        end
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/garethjevans/scm/releases/download/0.3.1/scm-linux-arm64.tar.gz"
-      sha256 "a7e21a1d401c3e5b136f9a3b06a14913bda3abb9ee8e146fc7e023d9bdaae6be"
+    if Hardware::CPU.arm?
+      if Hardware::CPU.is_64_bit?
+        url "https://github.com/garethjevans/scm/releases/download/0.3.3/scm-linux-arm64.tar.gz"
+        sha256 "babdb9dc7b6ba1b7b51d006d1aa8d6c05174dd1b6eac406ac9bedfba1abf3b82"
 
-      def install
-        bin.install "scm"
+        def install
+          bin.install "scm"
+        end
       end
     end
   end
